00e3ec13e3e964dc2a76becf3ca0bae999d3708c
Kumar Abhinav <abhinav0441@gmail.com>
2019-03-11 10:09:48 +0530

    aa

---
+++ 1-typescript/types.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+var person = 'Sam';
+var age = 23;
+var isIndian = false;
+var address = {
+    city: 'Banglore',
+    state: 'Karnataka'
+};
+var thing = 23;
+thing = 'something';
+var x;
+x = 33;
+x = 'aa';
---
+++ 1-typescript/types.ts
@@ -0,0 +1,18 @@
+console.log('Hello World');
+
+let person : string = 'Sam';
+let age : number = 23;
+let isIndian : boolean = false;
+let address : Object = {
+    city: 'Banglore',
+    state: 'Karnataka'
+};
+
+let thing : any = 23;
+thing = 'something';
+
+let x;
+x = 33;
+x = 'aa';
+
+
