8e60c14389d555d7375ea52108b6831669af7297
Kumar Abhinav <abhinav0441@gmail.com>
2019-03-11 11:52:40 +0530

    zz

--- 2-components/bookscart/src/app/components/home-page/home-page.component.ts
+++ 2-components/bookscart/src/app/components/home-page/home-page.component.ts
@@ -7,16 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  book: Book;
+  books: Book[];
 
-  constructor() { 
-    this.book = new Book(
-      'The Alchemist',
-      'Paulo Cohelo',
-      23,
-      4
-    );
-    console.log(this.book);
+  constructor() {
+    this.books = [
+      new Book(
+        'The Alchemist',
+        'Paulo Cohelo',
+        23,
+        4
+      ),
+      new Book(
+        'The monk who sold his ferrari',
+        'Robin Sharma',
+        43,
+        2
+      ),
+      new Book(
+        'Five point someone',
+        'Chetan Bhagat',
+        11,
+        1
+      ),
+      new Book(
+        '4 hour work week',
+        'Tim Ferris',
+        24,
+        5
+      )
+    ];
+  }
+
+  rateUp(book) {
+    if(book.rating < 5)
+      book.rating++;
+  }
+
+  rateDown(book) {
+    if(book.rating > 1)
+      book.rating--;
   }
 
   ngOnInit() {
